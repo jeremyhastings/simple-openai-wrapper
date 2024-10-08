61e90ddb422b011c83300febbc7acc0dcef91f24
Jeremy Hastings <jeremy.hastings@mac.com>
2024-10-08 11:47:05 -0500

    Add signatures to response_renderer.rb

--- lib/simple/openai/response_renderer.rb
+++ lib/simple/openai/response_renderer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "response_strategy"
+
 module Simple
   module Openai
     # The ResponseRenderer class is responsible for rendering the response using a specific strategy.
@@ -8,10 +10,11 @@ module Simple
       attr_reader :strategy
 
       # Initializes the ResponseRenderer with an optional strategy.
-      # If no strategy is provided, it will need to be set before rendering a response.
+      # If no strategy is provided, a default strategy will be used.
       #
-      # @param [Object, nil] strategy The strategy to be used for rendering the response. Default is nil.
-      def initialize(strategy = nil)
+      # @param [Simple::Openai::ResponseStrategy] strategy The strategy to be used for rendering the response.
+      #   Default is an instance of Simple::Openai::ResponseStrategy.
+      def initialize(strategy = Simple::Openai::ResponseStrategy.new)
         @strategy = strategy
       end
 
@@ -20,17 +23,21 @@ module Simple
       # @param [Object] response The response object to be rendered.
       # @raise [RuntimeError] If no strategy is set, raises an error indicating that a strategy must be set.
       def render_response(response)
-        raise "Strategy not set for ResponseRenderer" unless @strategy
+        raise "Strategy not set. Please set a strategy before rendering responses." unless @strategy
 
         @strategy.render_response(response)
       end
 
       # Sets a new strategy to be used for rendering responses.
       #
-      # @param [Object] new_strategy The new strategy to be used for rendering responses.
+      # @param [Simple::Openai::ResponseStrategy] new_strategy The new strategy to be used for rendering responses.
+      # @raise [ArgumentError] If the new strategy does not respond to `render_response`.
       # @return [void]
       def apply_strategy(new_strategy)
-        # Perform any validation or additional logic here if needed
+        unless new_strategy.respond_to?(:render_response)
+          raise ArgumentError, "New strategy must respond to `render_response`"
+        end
+
         @strategy = new_strategy
       end
     end
--- test/simple/openai/test_response_renderer.rb
+++ test/simple/openai/test_response_renderer.rb
@@ -2,6 +2,7 @@
 
 require_relative "../../test_helper"
 require_relative "../../../lib/simple/openai/response_renderer"
+require_relative "../../../lib/simple/openai/simple_text_response_strategy"
 
 # This test file contains unit tests for the Simple::Openai::ResponseRenderer class.
 # ResponseRenderer is responsible for rendering responses based on a provided strategy.
@@ -21,7 +22,7 @@ class ResponseRendererTest < Minitest::Test
   end
 
   def test_initialize_with_no_strategy
-    assert_nil response_renderer.strategy
+    assert_instance_of Simple::Openai::ResponseStrategy, response_renderer.strategy
   end
 
   def test_initialize_with_strategy
@@ -30,7 +31,7 @@ class ResponseRendererTest < Minitest::Test
   end
 
   def test_render_response_with_no_strategy
-    assert_raises(RuntimeError, "Strategy not set for ResponseRenderer") do
+    assert_raises(NotImplementedError, "You must implement the display_response method") do
       response_renderer.render_response({ key: "value" })
     end
   end
@@ -41,8 +42,9 @@ class ResponseRendererTest < Minitest::Test
     response_renderer.render_response({ key: "value" })
   end
 
-  def test_set_strategy
+  def test_apply_strategy
     new_strategy = Minitest::Mock.new
+    def new_strategy.render_response(response); end
     response_renderer.apply_strategy(new_strategy)
     assert_equal new_strategy.__id__, response_renderer.strategy.__id__
 
